dae1a157630fd002d48d6b418c55738fda07b2c9
HaiYen3012 <lehaiyen30122004@gmail.com>
2025-06-07 13:03:59 +0700

    update FE

--- client/src/components/layouts/MainLayout.jsx
+++ client/src/components/layouts/MainLayout.jsx
@@ -1,30 +1,23 @@
 import React from 'react'
-import { Breadcrumb, Layout, theme } from 'antd'
+import { Layout } from 'antd'
 import HeaderRender from 'components/header'
 import SiderRender from 'components/siderbar'
 import FooterRender from 'components/footer'
-const { Content, Sider, Header } = Layout
+import './main-layout.scss' // <-- IMPORT FILE SCSS MỚI Ở ĐÂY
+
+const { Content, Sider } = Layout
 
 const App = (props) => {
-  //   const {
-  //     token: { colorBgContainer, borderRadiusLG },
-  //   } = theme.useToken();
-  const theme = 'light'
+  const theme = 'light' // Giữ theme light để Ant Design không ghi đè màu tối
+
   return (
-    <Layout
-      style={{
-        minHeight: '100vh',
-      }}
-    >
+    <Layout className="main-layout-wrapper"> {/* <-- THÊM CLASS WRAPPER */}
       <HeaderRender />
-      <Layout>
+      <Layout className="main-layout-wrapper__body"> {/* <-- THÊM CLASS WRAPPER */}
         <SiderRender theme={theme} />
-        <Layout>
-          <Content
-            style={{
-              margin: '16px 16px',
-            }}
-          >
+        <Layout className="main-layout-wrapper__content-area"> {/* <-- THÊM CLASS WRAPPER */}
+          <Content className="main-layout-wrapper__content">
+            {/* Truyền component vào đây */}
             <props.component />
           </Content>
           <FooterRender />
